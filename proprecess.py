c41bc4fb5dc5378e67ecf9726827be885790e042
ShihengCao <1262653738@qq.com>
2024-11-07 14:16:46 +0800

    remove HW files

--- proprecess.py
+++ proprecess.py
@@ -6,8 +6,8 @@ Project_name = sys.argv[1]
 workspace_path = os.path.join("..", "GCoM_outputs")
 outputs_dir = os.path.join(workspace_path, Project_name)
 output_file = os.path.join(workspace_path, Project_name + ".csv")
-ground_truth_dir = os.path.join('.', "HW")
-ground_truth_file = os.path.join(ground_truth_dir, Project_name + "_10_m.csv")
+ground_truth_dir = os.path.join('..', "HW")
+ground_truth_file = os.path.join(ground_truth_dir, Project_name + "-bf16_10_m_processed.csv")
 
 files = os.listdir(outputs_dir)
 
